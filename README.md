54b2697e384cd91783fcff802c1c9f604150f226
Oliver Tan <otan@cockroachlabs.com>
2020-04-03 17:54:35 -0700

    add app mode

--- README.md
+++ README.md
@@ -2,26 +2,23 @@
 
 ## Developing Locally
 
-(Make sure your have python3.7 and virtualenv installed)[https://cloud.google.com/python/setup]
-
-* Get a GitHub Access Token. Then you can do this:
-```
-export BLATHERS_GITHUB_ACCESS_TOKEN=<your token here>
-```
-
-* Now we can run the following.
+* (Make sure your have python3.7 and virtualenv installed)[https://cloud.google.com/python/setup].
+* Get access! You have two options:
+  * Get a GitHub Access Token, then export it: `export BLATHERS_GITHUB_ACCESS_TOKEN=<your token here>`
+  * Use the bot token. base64 encode the private key, then export it: `export BLATHERS_GITHUB_PRIVATE_KEY=<your token here>`.
+* Now can run the following to start the server
 ```
 source venv/bin/activate
 pip3 install requirements.txt
 python3 main.py
 ```
-
 * Then CURL your webhook request to test.
 ```
 curl localhost:5000/webhook -H 'X-Github-Event: <event>' --header "Content-Type:application/json" --data '<data>'
 ```
 
-Then you can try CURL the webhook requests
+Example data is available in `example_webhooks/`
+
 
 ## Blathers Inspiration
 > "Eeek! A bug...! Ah, I beg your pardon! I just don't like handling these things much!"
--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import logging
 import json
+import jwt
 import os
 import requests
 import sys
 import re
+import base64
 from abc import ABC, abstractmethod
 from flask import Flask, abort, request as flask_request
 from collections import defaultdict
-
-assert 'BLATHERS_GITHUB_ACCESS_TOKEN' in os.environ
+from datetime import datetime, timedelta
 
 logging.basicConfig(stream=sys.stderr)
 
 application = Flask(__name__)
 
-headers = {"Authorization": "token " + os.environ['BLATHERS_GITHUB_ACCESS_TOKEN']}
+access_token = ("", datetime.now())
+def get_headers(installation_id):
+    global access_token
+    if installation_id and 'BLATHERS_GITHUB_PRIVATE_KEY' in os.environ:
+        if datetime.now() > access_token[1]:
+            encoded_jwt = jwt.encode(
+                {
+                    'iss': 59700,
+                    'iat': datetime.utcnow(),
+                    'exp': datetime.utcnow() + timedelta(seconds=600),
+                },
+                base64.b64decode(os.environ['BLATHERS_GITHUB_PRIVATE_KEY']),
+                algorithm='RS256',
+            ).decode('utf-8')
+            jwt_headers = {
+                'Authorization': 'Bearer {}'.format(encoded_jwt),
+                'Accept': 'application/vnd.github.machine-man-preview+json',
+            }
+            resp = requests.post(
+                'https://api.github.com/app/installations/{}/access_tokens'.format(installation_id),
+                headers=jwt_headers,
+            )
+            assert resp.status_code == 201, resp.content
+            ret = resp.json()
+            access_token = (ret['token'], datetime.strptime(ret['expires_at'], "%Y-%m-%dT%H:%M:%SZ"))
+        return {"Authorization": "token " + access_token[0]}
+    assert 'BLATHERS_GITHUB_ACCESS_TOKEN' in os.environ
+    return {"Authorization": "token " + os.environ['BLATHERS_GITHUB_ACCESS_TOKEN']}
 
 class Actions(ABC):
     def __init__(self):
@@ -40,7 +68,7 @@ class PullRequestActions(Actions):
     def add_reviewer(self, reviewer, reason):
         self.reviewers[reviewer].append(reason)
 
-    def do(self):
+    def do(self, installation_id):
         if not self.comments:
             return
         self.comments.append("NOTE: I am experimental.")
@@ -48,27 +76,26 @@ class PullRequestActions(Actions):
         logging.info('posting comment: %s', text)
         resp = requests.post(
             self.comments_url,
-            headers=headers,
+            headers=get_headers(installation_id),
             json={
                 "body": text,
             },
         )
-        if resp.status_code != 201:
-            raise Exception('unknown response from comment creation')
+        assert resp.status_code == 201, resp.content
 
 
-def is_organization_member(user_name):
-    resp = requests.get('https://api.github.com/orgs/cockroachdb/members/{}'.format(user_name), headers=headers)
+def is_organization_member(installation_id, user_name):
+    resp = requests.get('https://api.github.com/orgs/cockroachdb/members/{}'.format(user_name), headers=get_headers(installation_id))
     if resp.status_code == 204:
         return True
     if resp.status_code == 404:
         return False
-    raise Exception('unknown status code for is_organization_member: {}'.format(resp.status_code))
+    assert False, resp
 
-def list_commits(commits_url):
-    resp = requests.get(commits_url, headers=headers)
+def list_commits(installation_id, commits_url):
+    resp = requests.get(commits_url, headers=get_headers(installation_id))
     if resp.status_code != 200:
-        raise Exception('unable to fetch commits: {}'.format(resp))
+        assert False, resp.content
     return resp.json()
 
 issue_regexes = [
@@ -87,26 +114,34 @@ def get_associated_issues_from_text(text):
     return list(issues)
 
 def handle_pull_request(req):
-    # Let's only handle opened pull requests for now.
     logging.info('found pull_request %s', json.dumps(req))
-    if req['action'] not in ('opened'):
+    installation_id = req['installation']['id'] if 'installation' in req else None
+
+    if req['action'] not in ('opened', 'synchronize'):
         return json.dumps({'status': 'do not care about this event'})
 
-    if is_organization_member(req['pull_request']['user']['login']) and req['pull_request']['user']['login'] != 'otan':
+    if is_organization_member(installation_id, req['pull_request']['user']['login']) and req['pull_request']['user']['login'] != 'otan':
         return json.dumps({'status': 'not handled as person is member of the organization'})
 
     actions = PullRequestActions(req['pull_request'])
     if req['action'] == 'opened':
+        contributor_text_gate = 'Please ensure you have followed the guidelines for [creating a PR]'
+        if req['pull_request']['author_association'] == 'FIRST_TIME_CONTRIBUTOR':
+            contributor_text_gate = 'Since this is your first PR, please make sure you have read [this guide to creating your first PR]'
         actions.add_comment(
-            'Hello external contributor! I am Blathers and I am here to assist you get your PR merged. ' +
-            'If this is your first PR, please make sure you have read [this guide to creating your first PR](https://wiki.crdb.io/wiki/spaces/CRDB/pages/181633464/Your+first+CockroachDB+PR]).',
+            'Hello external contributor! I am Blathers and I am here to assist to get your PR merged. ' +
+            '{}(https://wiki.crdb.io/wiki/spaces/CRDB/pages/181633464/Your+first+CockroachDB+PR]).'.format(
+                contributor_text_gate,
+            ),
         )
+    else:
+        actions.add_comment('Thanks for updating your pull request.')
 
     # Check for action items.
     action_items = []
 
     # Check commits.
-    commits = list_commits(req['pull_request']['commits_url'])
+    commits = list_commits(installation_id, req['pull_request']['commits_url'])
     if len(commits) > 1:
         action_items.append('Generally we try and keep pull requests to one commit. [Please squash your commits](https://github.com/wprig/wprig/wiki/How-to-squash-commits), and re-push with `--force`.')
     for commit in commits:
@@ -117,32 +152,26 @@ def handle_pull_request(req):
         action_items.append('Please ensure your pull request description contains a [release note](https://wiki.crdb.io/wiki/spaces/CRDB/pages/186548364/Release+notes) - this can be the same as the one in your commit message.')
 
     if action_items:
+        action_items.append('When CI has completed, please ensure there are no errors that have appeared.')
         actions.add_comment(
             'Before we can accept your PR, please review the following recommendations: {}'.format(
                 ''.join(['\n * ' + item for item in action_items]),
             ),
         )
+    else:
+        actions.add_comment('My owl senses detect that your PR is good for review. Please keep an out for any test failures in CI.')
 
     # Automatically assign reviewers who can help.
     if len(req['pull_request']['requested_reviewers']) == 0:
         actions.add_comment('''We were unable to automatically find a reviewer for your issue. You can try CCing one of the following members:
 * A person you worked with closely on this PR.
-* The person who created the ticket, or a CRDB organization member involved with the ticket.
+* The person who created the ticket, or a [CRDB organization member](https://github.com/orgs/cockroachdb/people) involved with the ticket (author, commenter, etc.).
 * Join our [community slack channel](https://cockroa.ch/slack) and ask on #contributors.
-* You can find members of the CockroachDB organization [here](https://github.com/orgs/cockroachdb/people).''')
+* Try find someone else from [here](https://github.com/orgs/cockroachdb/people).''')
 
     # Apply actions and return.
-    actions.do()
+    actions.do(installation_id)
     return json.dumps({'status': 'handled pull request'})
-
-def handle_issue(req):
-    # Let's only handled opened issues for now.
-    logging.info('found issue %s', json.dumps(req))
-    if req['action'] != 'opened':
-        return json.dumps({'status': 'issue is not new'})
-    if is_organization_member(req['issue']['user']['login']):
-        return json.dumps({'status': 'not handled as person is member of the organization'})
-    return json.dumps({'status': 'handled issue'})
 
 @application.route('/webhook', methods=['POST'])
 def do_webhook():
