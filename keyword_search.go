8fbc6bbf64db009c480fe33e0ded5470c9cf588f
Oliver Tan <otan@cockroachlabs.com>
2021-03-15 10:18:01 +1100

    remove appdev

--- keyword_search.go
+++ keyword_search.go
@@ -3,26 +3,26 @@ package blathers
 import "regexp"
 
 var teamToKeyword = map[string][]string{
-	"appdev": {`(?i)\borm\b`, `ORM`, `(?i)django`, `(?i)hibernate`,
+	"bulk-io":    {`(?i)backup`, `(?i)import\b`, `(?i)export`, `(?i)cockroach dump`, `(?i)restore`, `(?i)changefeed`, `(?i)cdc`},
+	"sql-schema": {`(?i)(alter|drop)\s+(table|index|database)`, `(?i)(table|database|schema|index|column)\s+descriptor`},
+	"kv":         {`(?i)\bkv\b`, `(?i)HLC`, `(?i)raft`},
+	"sql-experience": {
+		`(?i)sql statement`, `(?i)join\b`, `(?i)pg_`,
+		`(?i)\borm\b`, `ORM`, `(?i)django`, `(?i)hibernate`,
 		`(?i)spring`, `(?i)\becto\b`, `(?i)activerecord`, `(?i)rails`, `(?i)sqlalchemy`,
 		`(?i)sequelize`, `(?i)jooq`, `(?i)pgjdbc`, `(?i)pgx`, `(?i)libpq`,
 		`(?i)cockroach-go`, `(?i)ponyorm`, `(?i)postico`, `(?i)peewee`, `(?i)flyway`,
 		`(?i)hasura`, `(?i)liquibase`, `(?i)gorm`, `(?i)alembic`, `(?i)knex`,
 		`(?i)prisma`, `(?i)psycopg2`, `(?i)tableplus`,
 	},
-	"bulk-io":        {`(?i)backup`, `(?i)import\b`, `(?i)export`, `(?i)cockroach dump`, `(?i)restore`, `(?i)changefeed`, `(?i)cdc`},
-	"sql-schema":     {`(?i)(alter|drop)\s+(table|index|database)`, `(?i)(table|database|schema|index|column)\s+descriptor`},
-	"kv":             {`(?i)\bkv\b`, `(?i)HLC`, `(?i)raft`},
-	"sql-experience": {`(?i)sql statement`, `(?i)join\b`, `(?i)pg_`},
-	"vectorized":     {`(?i)vectorize`},
-	"observability":  {`(?i)admin\s+ui\b`, `(?i)web\s+ui\b`, `(?i)db\s+console`},
-	"storage":        {`(?i)rocks\s*db`, `(?i)pebble`},
-	"optimizer":      {`(?i)explain(^ your problem)`, `(?i)plan`},
-	"cloud":          {`(?i)kubernetes`, `(?i)cloud`},
+	"vectorized":    {`(?i)vectorize`},
+	"observability": {`(?i)admin\s+ui\b`, `(?i)web\s+ui\b`, `(?i)db\s+console`},
+	"storage":       {`(?i)rocks\s*db`, `(?i)pebble`},
+	"optimizer":     {`(?i)explain(^ your problem)`, `(?i)plan`},
+	"cloud":         {`(?i)kubernetes`, `(?i)cloud`},
 }
 
 var teamToContacts = map[string][]string{
-	"appdev":         {`rafiss`},
 	"bulk-io":        {`cockroachdb/bulk-io`},
 	"sql-schema":     {`ajwerner`, `jordanlewis`},
 	"kv":             {`nvanbenschoten`},
@@ -42,7 +42,6 @@ type board struct {
 }
 
 var teamToBoards = map[string]board{
-	"appdev":         {"cockroachdb", "", "AppDev", "Triage"},
 	"bulk-io":        {"cockroachdb", "cockroach", "Bulk I/O Backlog", "Triage"},
 	"sql-schema":     {"cockroachdb", "cockroach", "SQL Schema", "Triage"},
 	"kv":             {"cockroachdb", "cockroach", "KV Backlog", "Incoming"},
