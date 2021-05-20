1284564d4aba9b7472cb84e16297f4391d7166a1
Matt Sherman <sherman@cockroachlabs.com>
2021-05-20 13:44:26 -0400

    fmt

--- github_webhook.go
+++ github_webhook.go
@@ -15,11 +15,11 @@ import (
 // blacklistedLogins contains a list of organization members to
 // ignore in certain situations.
 var blacklistedLogins = map[string]struct{}{
-	"cockroach-teamcity":   struct{}{},
-	"cockroach-oncall":     struct{}{},
-	"cockroach-roachdash":  struct{}{},
-	"crl-monitor-roach":    struct{}{},
-	"exalate-issue-sync":   struct{}{},
+	"cockroach-teamcity":  struct{}{},
+	"cockroach-oncall":    struct{}{},
+	"cockroach-roachdash": struct{}{},
+	"crl-monitor-roach":   struct{}{},
+	"exalate-issue-sync":  struct{}{},
 }
 
 // listBuilder keeps track of action items needed to be done.
@@ -274,9 +274,9 @@ func (srv *blathersServer) handleIssueOpened(ctx context.Context, event *github.
 		installationID(event.Installation.GetID()),
 	)
 
-    // These bots all have their own logic to label and tag issues.
+	// These bots all have their own logic to label and tag issues.
 	if _, isBlacklistedLogin := blacklistedLogins[event.GetSender().GetLogin()]; isBlacklistedLogin {
-        writeLogf(ctx, "skipping issue %s as member %s is blacklisted", event.GetIssue(), event.GetSender().GetLogin())
+		writeLogf(ctx, "skipping issue %s as member %s is blacklisted", event.GetIssue(), event.GetSender().GetLogin())
 		return nil
 	}
 
